5661a6d85e04d3b06f1f1b2b028b8ad43d5dbb05
vick0088 <60358348+vick0088@users.noreply.github.com>
2023-03-30 23:50:57 +0200

    Changed the shaders' file names, and created the toon shader class

--- src/application.cpp
+++ src/application.cpp
@@ -70,9 +70,9 @@ class Application {
 		try {
 			ShaderBuilder defaultBuilder;
 			defaultBuilder.addStage(GL_VERTEX_SHADER,
-									"shaders/new_shader_vert.glsl");
+									"shaders/phong_shader_vert.glsl");
 			defaultBuilder.addStage(GL_FRAGMENT_SHADER,
-									"shaders/toon_shader_frag.glsl");
+									"shaders/phong_shader_frag.glsl");
 			m_defaultShader = defaultBuilder.build();
 
 			ShaderBuilder shadowBuilder;
@@ -80,6 +80,12 @@ class Application {
 								   "shaders/shadow_vert.glsl");
 			m_shadowShader = shadowBuilder.build();
 
+
+			ShaderBuilder toonBuilder;
+			toonBuilder.addStage(GL_VERTEX_SHADER,
+												 "shaders/phong_shader_vert.glsl");
+			toonBuilder.addStage(GL_FRAGMENT_SHADER, "shaders/toon_shader_frag.glsl");
+			m_toonShader = toonBuilder.build();
 			// Any new shaders can be added below in similar fashion.
 			// ==> Don't forget to reconfigure CMake when you do!
 			//     Visual Studio: PROJECT => Generate Cache for ComputerGraphics
@@ -127,7 +133,7 @@ class Application {
 			glUniform3fv(9, 1, glm::value_ptr(m_materialDiffuse));
 			glUniform3fv(10, 1, glm::value_ptr(m_materialSpecular));
 			glUniform1f(11, materialShininess);
-			glUniform4fv(12, 1, glm::value_ptr(texColor));
+			//glUniform4fv(12, 1, glm::value_ptr(texColor));
 
 
 			// movement logic main character/mesh_1
@@ -304,6 +310,7 @@ class Application {
 	// Shader for default rendering and for depth rendering
 	Shader m_defaultShader;
 	Shader m_shadowShader;
+	Shader m_toonShader;
 
 	GPUMesh m_mesh;
 	GPUMesh m_mesh2;
