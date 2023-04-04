5498aa6cbc8cb8f9009219d94d7f74c493ec14e7
vick0088 <60358348+vick0088@users.noreply.github.com>
2023-04-04 04:12:44 +0200

    Revert "snake"
    This reverts commit 92636b7367231a358476236b8356d270d3dc3764.

--- enc_temp_folder/32253c6aec1fc14bda38b0daa5c1c7b6/player.cpp
+++ enc_temp_folder/32253c6aec1fc14bda38b0daa5c1c7b6/player.cpp
@@ -105,11 +105,6 @@ class Player {
 				glUniformMatrix3fv(2, 1, GL_FALSE, glm::value_ptr(normalModelMatrix));
 
 			}
-			drawCorrectModel(framecounter);
-			
-		}
-
-		void drawCorrectModel(int framecounter) {
 			// awful animation implementation :(
 			switch ((framecounter % 80) / 2) {
 			case 0:
@@ -194,12 +189,7 @@ class Player {
 				break;
 
 			}
-		}
-
-		void shadowDraw(glm::mat4 m_projectionMatrix, glm::vec3 lightPos, int framecounter) {
-			const glm::mat4 mvp = m_projectionMatrix * glm::lookAt(lightPos, glm::vec3(0.0), glm::vec3(0, 1, 0)) * m_modelMatrix;
-			glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(mvp));
-			drawCorrectModel(framecounter);
+			
 		}
 
 	private:
---
+++ enc_temp_folder/9fb79f70352ed2d1b2b58d10c61410/bullet.cpp
@@ -0,0 +1,74 @@
+#include "mesh.h"
+#include <GLFW/glfw3.h>
+#include <imgui/imgui.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_inverse.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <list>
+#include <iostream>
+#include <glm/gtx/string_cast.hpp>
+
+
+
+
+class Bullet {
+	public:
+		Bullet(glm::mat4 startMatrix, int speed, int damage, int range):
+			speed(speed),
+			damage(damage),
+			range(range),
+			m_modelMatrix(startMatrix)
+		{
+			/*
+			for(int x = 0; x < 4 ; x++){
+				for (int y = 0; y < 4; y++) {
+					float* temp = &(startMatrix[x][y]);
+					m_modelMatrix[x][y] = (*temp);
+
+				}
+			}
+			*/
+
+		}
+
+		glm::vec3 getLocation() {
+			return m_modelMatrix * glm::vec4(0, 0, 0, 1);
+		}
+
+		bool outOfRange(glm::vec3 location) {
+			if (glm::length(getLocation() - location) > 2000) {
+				return true;
+			}
+			return false;
+		}
+
+		void draw(glm::mat4 m_projectionMatrix, glm::mat4 m_viewMatrix, GPUMesh *m_mesh, Texture *m_texture) {
+			m_modelMatrix = glm::translate(m_modelMatrix, glm::vec3(0.0f, 0.0f, -0.3f));
+			//std::cout << glm::to_string(getLocation()) << "\n";
+			std::cout << speed << "\n";
+			const glm::mat4 mvpMatrix = m_projectionMatrix * m_viewMatrix * m_modelMatrix;
+			const glm::mat3 normalModelMatrix = glm::inverseTranspose(glm::mat3(m_modelMatrix));
+
+			glUniformMatrix4fv(0, 1, GL_FALSE, glm::value_ptr(mvpMatrix));
+			glUniformMatrix4fv(1, 1, GL_FALSE, glm::value_ptr(m_modelMatrix));
+			glUniformMatrix3fv(2, 1, GL_FALSE, glm::value_ptr(normalModelMatrix));
+			if ((*m_mesh).hasTextureCoords()) {
+				(*m_texture).bind(GL_TEXTURE1);
+				//m_mesh2.kdTexture.value().bind(GL_TEXTURE1);
+				glUniform1i(3, 1);
+				glUniform1i(4, GL_TRUE);
+			}
+			else {
+				glUniform1i(4, GL_FALSE);
+			}
+			(*m_mesh).draw();
+		}
+	private:
+		glm::mat4 m_modelMatrix;
+
+		int speed;
+		int damage;
+		int range;
+
+};
